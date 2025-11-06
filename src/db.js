400229dd17bf44b9cc22802b8f6bb8939fa87ae0
Ejimurphy <87836653+Ejimurphy@users.noreply.github.com>
2025-11-06 11:47:49 +0100

    Update db.js

--- src/db.js
+++ src/db.js
@@ -1,12 +1,12 @@
 import pkg from "pg";
 const { Pool } = pkg;
-import dotenv from "dotenv";
-dotenv.config();
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: { rejectUnauthorized: false },
 });
+
+export default pool;
 
 // --- AUTO CREATE TABLES ---
 const initTables = async () => {
