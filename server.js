8d904cb40ec67f7c1a1addf3f03451ab3d79bd90
Ejimurphy <87836653+Ejimurphy@users.noreply.github.com>
2025-11-06 12:40:43 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
-import pool from "./src/db.js"; // ✅ import the Pool connection from db.js
+// ✅ import the Pool connection from db.js
 
 dotenv.config();
 
