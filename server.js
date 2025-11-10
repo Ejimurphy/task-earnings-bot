0626a58bd6db6eb0e361d408baf7d003fc429814
Ejimurphy <87836653+Ejimurphy@users.noreply.github.com>
2025-11-10 13:59:40 +0100

    Update server.js

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ import express from "express";
 import dotenv from "dotenv";
 import { Telegraf, Markup, session } from "telegraf";
 
+let performTaskEnabled = true; // ✅ Default: ON
+
 // ✅ Load environment variables
 dotenv.config();
 
