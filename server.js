4390050e1dfca6ef06a9337d32c04df8f94bf877
Ejimurphy <87836653+Ejimurphy@users.noreply.github.com>
2025-11-15 09:00:58 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import { Telegraf, Markup, session } from "telegraf";
+import { safeQuery } from "./src/utils.js";
 
 let performTaskEnabled = true; // ✅ Default: ON
 
